c915ad351ea565c67cdf2ccb7a7012c65f0f2f6c
Nima Moshtagh <nima.moshtagh1@gmail.com>
2020-02-09 17:10:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,20 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index{|item, index| p "#{index+1}:#{item}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(elements)
+  elements.collect{|word| word.capitalize + '!'}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(plants)
+  statement = false
+  if plants.collect{|word| word.length<4}
+    statement
+  end
+  if plants.collect{|word| word.length>4}
+    statement = true
+  end
+  statement
 end
 
 def find_the_cheese# code an argument here
